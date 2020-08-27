f216c98de2592463bf231bcd5ed7b933df848900
myjamong <44043666+myJamong@users.noreply.github.com>
2020-08-27 18:33:27 +0900

    Create 11052_카드_구매하기.py

---
+++ dynamic_programming/11052_카드_구매하기.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/11052
+# 카드 구매하기
+
+import sys
+if __name__ == "__main__":
+    n = int(sys.stdin.readline())
+    arr = list(map(int,sys.stdin.readline().split()))
+    arr.insert(0,0)
+    dp = [0] * (n+1)
+    dp[1] = arr[1]
+    for i in range(2,n+1):
+        temp = [arr[i]]
+        for j in range(1,i+1):
+            temp.append(dp[i-j]+arr[j])
+        dp[i] = max(temp)
+    print(dp[n])
