d1d18e047117a70cf84766397031c8cbea39fd04
myjamong <44043666+myJamong@users.noreply.github.com>
2020-09-01 11:28:39 +0900

    Update 2293_동전1.py

--- dynamic_programming/2293_동전1.py
+++ dynamic_programming/2293_동전1.py
@@ -3,12 +3,12 @@
 
 import sys
 if __name__ == "__main__":
-    n,k = map(int,sys.stdin.readline().split())
+    read = sys.stdin.readline
+    n,k = map(int,read().split())
+    coins = [int(read()) for _ in range(n)]
     dp = [0] * (k+1)
-    dp[0] = 1
-    for i in range(n):
-        val = int(sys.stdin.readline())
-        for j in range(val,k+1):
-            if j - val >= 0:
-                dp[j] += dp[j-val]
+    dp[0] = 1 # 동전 자기 자신을 위한 count
+    for coin in coins:
+        for j in range(coin,k+1):
+            dp[j] += dp[j-coin] # 여태까지 사용한 동전으로 j를 만들수 있는 방법 dp[j]에다가 현재 동전 하나를 더해서 사용할 수 있는 새로운 방법 dp[j-coin] 더함
     print(dp[k])
