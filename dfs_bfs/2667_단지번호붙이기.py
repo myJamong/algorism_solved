a8f3c01f707c99e3f0c6dcf37afd58452b8ddea5
myjamong <44043666+myJamong@users.noreply.github.com>
2020-09-02 16:46:16 +0900

    Update 2667_단지번호붙이기.py

--- dfs_bfs/2667_단지번호붙이기.py
+++ dfs_bfs/2667_단지번호붙이기.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
     res = []
     for i in range(n):
         for j in range(n):
-            if arr[i][j] == '1':
-                total += 1
+            if arr[i][j] == '1': # 아파트 있는지 확인
+                total += 1 # 단지 수 +1
                 q = deque()
                 q.append((i,j))
-                arr[i][j] = '0'
+                arr[i][j] = '0' # 처음 확인한 값은 0 으로 --> 단지에 아파트가 1개일때 반례 --> 다시 확인하지 않게 하기 위해
                 cnt = 0
                 while q:
                     now = q.popleft()
@@ -25,7 +25,7 @@ if __name__ == "__main__":
                     for d in range(4):
                         h = now[0] + dh[d]
                         w = now[1] + dw[d]
-                        if 0 <= h < n and 0 <= w < n and arr[h][w] == '1':
+                        if 0 <= h < n and 0 <= w < n and arr[h][w] == '1': # 아파트인지 확인
                             arr[h][w] = '0'
                             q.append((h,w))
                 res.append(cnt)
