7bb48149705b260c5797f21eeb5e0abc8435623d
myjamong <44043666+myJamong@users.noreply.github.com>
2020-09-11 00:14:25 +0900

    Update 1202_보석_도둑.py

--- greedy/1202_보석_도둑.py
+++ greedy/1202_보석_도둑.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         while jewels and bag >= jewels[0][0]: # 가방 안에 보석이 들어갈 수 있는 경우
             m,v = heapq.heappop(jewels) # 보석 최소힙에서 빼고
             heapq.heappush(temp,-v) # 보석 최대힙으로 이동
-        if temp: --> 이거를 확인해주는게 가방은 있는데 보석이 없는 경우가 있고 더 작은 가방에 제일 큰 보석을 넣은 경우 더 큰 가방에 나머지 남은 보석들을 순차적으로 넣어줘야한다.
+        if temp: #--> 이거를 확인해주는게 가방은 있는데 보석이 없는 경우가 있고 더 작은 가방에 제일 큰 보석을 넣은 경우 더 큰 가방에 나머지 남은 보석들을 순차적으로 넣어줘야한다.
             result -= heapq.heappop(temp) # 가방에 넣을 수 있는 보석이 존재한다면 결과에 합친다.
         elif not jewels:
             break        
